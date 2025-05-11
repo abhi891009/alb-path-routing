8a0b8c92241dc8a804ce0369a039e1dd68f8ff61
abhi891009 <33829011+abhi891009@users.noreply.github.com>
2025-05-11 20:53:13 +0000

    Commiting the CI/CD pipeline

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -5,15 +5,14 @@ resource "aws_instance" "web" {
   subnet_id     = element(var.subnet_ids, count.index)
   security_groups = [var.alb_security_group_id]
 
-  user_data = <<-EOF
-              #!/bin/bash
-              echo 'Hello from EC2 instance ${count.index + 1}' > /var/www/html/index.html
-              yum install -y nginx
-              systemctl start nginx
-              systemctl enable nginx
-              EOF
-
-  tags = {
-    Name = "WebServer-${count.index + 1}"
+  
+user_data = <<-EOF
+              #!/bin/bash
+              apt update -y
+              apt install -y nginx
+              echo 'Hello from EC2 instance ${count.index + 1}' > /var/www/html/index.html
+              systemctl start nginx
+              systemctl enable nginx
+              EOF
   }
 }
