2321fa907ebdbd4e300d56ba65f56a64ec414bcd
abhi891009 <33829011+abhi891009@users.noreply.github.com>
2025-05-15 11:38:39 +0000

    Removed ASG

--- modules/asg/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "asg_name" {
-  description = "Name of the Auto Scaling Group"
-  value       = aws_autoscaling_group.example.name
-}
-
-output "launch_template_id" {
-  description = "ID of the Launch Template"
-  value       = aws_launch_template.example.id
-}
